7652d1e6997e694b2ee4dc48f35af7389eab8499
code-palyer-x <1326037525@qq.com>
2021-05-07 23:01:32 +0800

    1486！

---
+++ 力扣刷题/1486.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int sumXor(int x) {
+        if (x % 4 == 0) {
+            return x;
+        }
+        if (x % 4 == 1) {
+            return 1;
+        }
+        if (x % 4 == 2) {
+            return x + 1;
+        }
+        return 0;
+    }
+
+    int xorOperation(int n, int start) {
+        int s = start >> 1, e = n & start & 1;
+        int ret = sumXor(s - 1) ^ sumXor(s + n - 1);
+        return ret << 1 | e;
+    }
+};
+
+
+//https ://leetcode-cn.com/problems/xor-operation-in-an-array/solution/shu-zu-yi-huo-cao-zuo-by-leetcode-solution/
