4c3daad4fd65efd24c27de24c024469c3030e4c0
code-palyer-x <1326037525@qq.com>
2021-05-12 22:25:16 +0800

    1310

---
+++ 力扣刷题/1310.cpp
@@ -0,0 +1,23 @@
+//1310. 子数组异或查询
+
+#include<vector>
+using namespace std;
+
+
+
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        int n = arr.size();
+        vector<int> xors(n + 1);
+        for (int i = 0; i < n; i++) {
+            xors[i + 1] = xors[i] ^ arr[i];
+        }
+        int m = queries.size();
+        vector<int> ans(m);
+        for (int i = 0; i < m; i++) {
+            ans[i] = xors[queries[i][0]] ^ xors[queries[i][1] + 1];
+        }
+        return ans;
+    }
+};
