93efd1b1ea5729067249043d8d291b17951e26e3
code-player-x <1326037525@qq.com>
2021-06-11 22:46:30 +0800

    279

---
+++ 力扣刷题/279.cpp
@@ -0,0 +1,15 @@
+//279https://leetcode-cn.com/problems/perfect-squares/
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> f(n + 1);
+        for (int i = 1; i <= n; i++) {
+            int minn = INT_MAX;
+            for (int j = 1; j * j <= i; j++) {
+                minn = min(minn, f[i - j * j]);
+            }
+            f[i] = minn + 1;
+        }
+        return f[n];
+    }
+};
